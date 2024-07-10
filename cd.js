f8c74c1291066593c98a08e75f047f937416bdaf
ap-birtan <158799925+ap-birtan@users.noreply.github.com>
2024-07-10 13:09:29 +0600

    Add files via upload

---
+++ cd.js
@@ -0,0 +1,43 @@
+function isleap(year) {
+	if(year%400==0)
+	{
+       return true;
+	}
+	else if(year%4==0&&year%100!=0)
+	{
+       return true;
+	}
+	else
+		return false;
+}
+
+let cal=document.getElementById("cal");
+const days=[31,29,31,30,31,30,31,31,30,31,30,31];
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let mon=new Date().getMonth();
+document.getElementById("month").innerHTML=months[mon];
+for(let i=1;i<=days[mon];i++)
+{
+
+	cal.innerHTML+='<button class="date" onclick="tog(bm'+i+')" id="bm'+i+'">'+i+'<br>সকাল</button>';
+	cal.innerHTML+='<button class="date" onclick="tog(bn'+i+')" id="bn'+i+'">'+i+'<br>দুপুর</button>';
+	cal.innerHTML+='<button class="date" onclick="tog(bt'+i+')" id="bt'+i+'">'+i+'<br>রাত</button>';
+
+
+	if(i%4==0)
+	{
+	   cal.innerHTML+="<br>";	
+	}
+}
+
+function tog(bid) {
+
+if(bid.style.background=="green"||bid.style.background=="")
+	bid.style.background="red";
+else
+	bid.style.background="green";
+
+	
+
+
+}
